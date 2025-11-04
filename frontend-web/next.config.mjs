21d1572d5cc59c68528ef6d9e59b260f9e18b0e8
이승휘 <sunkus711@naver.com>
2025-11-04 13:31:01 +0900

    3

--- frontend-web/next.config.mjs
+++ frontend-web/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      canvas: false,
+    };
+
+    config.resolve.fallback = {
+      ...(config.resolve.fallback ?? {}),
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
